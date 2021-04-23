b082de6cec2d8d6aa72d6544eb80b03efe23a6a1
ET <tovareric@gmail.com>
2021-04-22 23:23:29 -0400

    fixed nav pages

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -3,13 +3,6 @@ import '../../App.css'
 
 
 
-
- function Products() {
-    
-    return (
-    <div>
-        <a href="https://us1.list-manage.com/contact-form?u=6b4ea06472d2b1a199b42bbd3&form_id=7e979584c4c408d0f7ad77f25bb56362" target="_blank" rel="noopener noreferrer">Connect</a>
-    </div>
-    )
+export default function Products() {
+    return <h1 className='products'><a href="https://us1.list-manage.com/contact-form?u=6b4ea06472d2b1a199b42bbd3&form_id=7e979584c4c408d0f7ad77f25bb56362" target="_blank" rel="noopener noreferrer">Connect</a></h1>;
 }
-export default Products;
