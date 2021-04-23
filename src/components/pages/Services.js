58243f1769f2db5365bac5e95454efffbe8ac120
ET <tovareric@gmail.com>
2021-04-22 23:50:01 -0400

    added list for resources

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import '../../App.css'
+import SimpleList from '../List'
 
 
 
 export default function Services() {
-    return <h1 className='services'>SERVICES</h1>;
+    return (
+    <SimpleList />
+    
+    );
 }
